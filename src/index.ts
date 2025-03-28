bed8e80621044ffeae75ef877f523256752edfd9
Maleesha Gimshan <47087449+maleeshagimshan98@users.noreply.github.com>
2025-03-28 15:30:22 +0530

    removed the index.js and converted it into index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Page, PageData } from './page';
+import vuexStateWrapper from './vuexStateWrapper';
+import { vuexPaginationWrapper } from './vuexPaginationWrapper';
+
+export { Page, PageData, vuexStateWrapper, vuexPaginationWrapper };
